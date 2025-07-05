8a213a774ba5725502787071cff0149ecda11345
zohaibmemon1515 <zohaibmemon1515@gmail.com>
2025-07-05 12:29:23 +0500

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,9 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/Navbar/page";
 import Footer from "./components/Footer/page";
+import Script from "next/script"; // ✅ Import Script
 
+// Local fonts
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -15,11 +17,13 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+// Metadata for the site
 export const metadata: Metadata = {
   title: "Static Blog Web",
   description: "Created by Zohaib Memon",
 };
 
+// Root layout component
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -27,12 +31,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3198948853332282"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar/>
+        <Navbar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
   );
